5cd7e94371b7e3d07e7b4752cfe08c5c61b2dacb
Ryan <ryancargan@gmail.com>
2022-10-31 06:56:02 +0530

    Add trust settings for postgres & add timer apps

--- configuration.nix
+++ configuration.nix
@@ -265,6 +265,13 @@ in
     timescaledb
     # age
   ];
+  services.postgresql.authentication = lib.mkForce ''
+    # Generated file; do not edit!
+    # TYPE  DATABASE        USER            ADDRESS                 METHOD
+    local   all             all                                     trust
+    host    all             all             127.0.0.1/32            trust
+    host    all             all             ::1/128                 trust
+  '';
 
   #-------------------------------------------------------------------------
   # Enable redis service
@@ -630,6 +637,10 @@ in
     gnupg
     xfsprogs
     parted
+
+    # Productivity tools
+    pomotroid
+    gnome.pomodoro
 
     # Bluetooth
     # obexftp
