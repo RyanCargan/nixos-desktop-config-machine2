e08c4e071b87d9dfd916f076f956375b099cbb73
Ryan <ryancargan@gmail.com>
2023-02-11 22:28:31 +0530

    Switch back to pulseaudio

--- configuration.nix
+++ configuration.nix
@@ -233,66 +233,70 @@ in
   # services.printing.enable = true;
 
   # Enable sound.
-  sound.enable = false;
-  # hardware.pulseaudio.package = pkgs.pulseaudioFull.override { bluetoothSupport = false; };
+  sound.enable = true;
+  hardware.pulseaudio.package = pkgs.pulseaudioFull.override { bluetoothSupport = false; };
   # hardware.pulseaudio.package = pkgs.pulseaudioFull;
-  # hardware.pulseaudio.enable = true;
-  # hardware.pulseaudio.support32Bit = true;
-  # nixpkgs.config.pulseaudio = true;
+  hardware.pulseaudio.enable = true;
+  hardware.pulseaudio.support32Bit = true;
+  nixpkgs.config.pulseaudio = true;
+  hardware.pulseaudio.extraConfig = ''
+    load-module module-combine-sink
+    unload-module module-suspend-on-idle
+  '';
 
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    jack.enable = true;
+ # security.rtkit.enable = true;
+ # services.pipewire = {
+ #   enable = true;
+ #   alsa.enable = true;
+ #   alsa.support32Bit = true;
+ #   pulse.enable = true;
+ #   # If you want to use JACK applications, uncomment this
+ #   jack.enable = true;
 
-    config.pipewire = {
-      "context.properties" = {
-        "link.max-buffers" = 16;
-        "log.level" = 2;
-        "default.clock.rate" = 48000;
-        "default.clock.quantum" = 128;
-        "default.clock.min-quantum" = 128;
-        "default.clock.max-quantum" = 128;
-        "core.daemon" = true;
-        "core.name" = "pipewire-0";
-        "session.suspend-timeout-seconds" = 0;
-      };
-      "context.modules" = [
-        {
-          name = "libpipewire-module-rtkit";
-          args = {
-            "nice.level" = -15;
-            "rt.prio" = 88;
-            "rt.time.soft" = 200000;
-            "rt.time.hard" = 200000;
-          };
-          flags = [ "ifexists" "nofail" ];
-        }
-        { name = "libpipewire-module-protocol-native"; }
-        { name = "libpipewire-module-profiler"; }
-        { name = "libpipewire-module-metadata"; }
-        { name = "libpipewire-module-spa-device-factory"; }
-        { name = "libpipewire-module-spa-node-factory"; }
-        { name = "libpipewire-module-client-node"; }
-        { name = "libpipewire-module-client-device"; }
-        {
-          name = "libpipewire-module-portal";
-          flags = [ "ifexists" "nofail" ];
-        }
-        {
-          name = "libpipewire-module-access";
-          args = {};
-        }
-        { name = "libpipewire-module-adapter"; }
-        { name = "libpipewire-module-link-factory"; }
-        { name = "libpipewire-module-session-manager"; }
-      ];
-    };
-  };
+ #   config.pipewire = {
+ #     "context.properties" = {
+ #       "link.max-buffers" = 16;
+ #       "log.level" = 2;
+ #       "default.clock.rate" = 48000;
+ #       "default.clock.quantum" = 128;
+ #       "default.clock.min-quantum" = 128;
+ #       "default.clock.max-quantum" = 128;
+ #       "core.daemon" = true;
+ #       "core.name" = "pipewire-0";
+ #       "session.suspend-timeout-seconds" = 0;
+ #     };
+ #     "context.modules" = [
+ #       {
+ #         name = "libpipewire-module-rtkit";
+ #         args = {
+ #           "nice.level" = -15;
+ #           "rt.prio" = 88;
+ #           "rt.time.soft" = 200000;
+ #           "rt.time.hard" = 200000;
+ #         };
+ #         flags = [ "ifexists" "nofail" ];
+ #       }
+ #       { name = "libpipewire-module-protocol-native"; }
+ #       { name = "libpipewire-module-profiler"; }
+ #       { name = "libpipewire-module-metadata"; }
+ #       { name = "libpipewire-module-spa-device-factory"; }
+ #       { name = "libpipewire-module-spa-node-factory"; }
+ #       { name = "libpipewire-module-client-node"; }
+ #       { name = "libpipewire-module-client-device"; }
+ #       {
+ #         name = "libpipewire-module-portal";
+ #         flags = [ "ifexists" "nofail" ];
+ #       }
+ #       {
+ #         name = "libpipewire-module-access";
+ #         args = {};
+ #       }
+ #       { name = "libpipewire-module-adapter"; }
+ #       { name = "libpipewire-module-link-factory"; }
+ #       { name = "libpipewire-module-session-manager"; }
+ #     ];
+ #   };
+ # };
 
   # Remove sound.enable or turn it off if you had it set previously, it seems to cause conflicts with pipewire
   #sound.enable = false;
@@ -480,7 +484,7 @@ in
 
     # Audio & video comms
     # droidcam
-    # (mumble.override { pulseSupport = true; })
+    (mumble.override { pulseSupport = true; })
     # murmur
     iproute2
     jq
