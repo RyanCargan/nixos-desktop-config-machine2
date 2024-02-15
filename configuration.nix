2cdc5f0fd3082dc720ed66441c78a35a2db4a9ce
Ryan <ryancargan@gmail.com>
2024-02-15 16:03:36 +0530

    Backup

--- configuration.nix
+++ configuration.nix
@@ -347,7 +347,7 @@ in
     remmina
 
     # Flakes
-    inputs.blender.packages.x86_64-linux.default
+    inputs.blender.packages.x86_64-linux.blender_3_6
     inputs.poetry2nix.packages.x86_64-linux.poetry2nix
     release2105.dos2unix
 
